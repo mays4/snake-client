72bbed15fdff68671b20fdcf64e3cf5f3e91d0a9
mays4 <maysalani84@yahoo.com>
2021-09-22 17:29:16 -0400

    play snake

--- play.js
+++ play.js
@@ -1,23 +1,13 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.223.133' ,// IP address here,
-    port: 50542// PORT number here,
-  });
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-  // interpret incoming data as text
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-   
-    console.log("hi player");
-  });
-  conn.setEncoding("utf8");
-  
-  return conn;
-  
-};
 console.log("Connecting ...");
-connect();
+//connect();
+setupInput(connect());
+
+
+
+
+
 
